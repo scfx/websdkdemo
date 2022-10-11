b9dd6b714b739483da63e2f6b9cf31d0a249819e
Felix Schaede <felix.schaede@softwareag.com>
2022-10-11 11:33:18 +0000

    Add basic device list

--- cockpit/src/navigator/custom-navigator.component.ts
+++ cockpit/src/navigator/custom-navigator.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { InventoryService, IManagedObject, IResultList } from '@c8y/client';
 
 @Component({
     selector: 'custom-navigator',
     templateUrl: './custom-navigator.component.html',
     //encapsulation: ViewEncapsulation.None
 })
-export class CustomNavigatorComponent { }
+export class CustomNavigatorComponent {
+    devices: IResultList<IManagedObject>;
+    constructor(private inventory: InventoryService) {
+        // _ annotation to mark this string as translatable string.
+        this.loadDevices();
+    }
+
+    private filter: object = {
+        fragmentType: 'c8y_IsDevice',
+        // paging information will be a part of the response now
+        withTotalPages: true,
+        pageSize: 10
+    };
+
+    async loadDevices() {
+        this.devices = await this.inventory.list(this.filter);
+    }
+
+}
